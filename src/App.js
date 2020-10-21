0ba4d9540c3cd2b81b57aed1c030bcbd05baceaf
Himanshu Sharma <32563057+hunnysharma@users.noreply.github.com>
2020-10-21 12:27:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="title">Hunny Photo Search</h1>
+        <h1 className="title">React Photo Search</h1>
+         <p>Using Unsplash API</p>
         <SearchPhotos />
 
       </div>
